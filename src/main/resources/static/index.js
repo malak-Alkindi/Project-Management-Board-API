232bd713f2bf293c2a24300c87d933a63449ff65
malak-Alkindi <120641708+malak-Alkindi@users.noreply.github.com>
2023-08-05 15:51:32 +0400

    add card through ui/api

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -81,58 +81,83 @@ window.addEventListener('click', (event) => {
 
 cardpopupForm.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
-    const card = document.createElement('div');
-    card.classList.add('card');
-  
-    const stateBar = document.createElement('div');
-    stateBar.classList.add('state-bar');
-  
-    const cardContent = document.createElement('div');
-    cardContent.classList.add('card-content');
-  
-    const cardIdElement = document.createElement('div');
-    cardIdElement.classList.add('card-id');
-    cardIdElement.innerText = '#' ;
-  
-    const cardTitleElement = document.createElement('h3');
-    cardTitleElement.classList.add('card-title');
-    cardTitleElement.innerText = document.getElementById('cardname').value.trim();
-  
-    const cardDescriptionElement = document.createElement('p');
-    cardDescriptionElement.classList.add('card-description');
-   
-    cardDescriptionElement.innerText = document.getElementById('carddescription').value;
-  
-    const actions = document.createElement('div');
-    actions.classList.add('actions');
-  
-    const updateBtn = document.createElement('button');
-    updateBtn.classList.add('update-btn');
-    updateBtn.innerText = 'Update';
-  
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.innerText = 'Delete';
-  
-    // Append all elements to the card
-    card.appendChild(stateBar);
-    card.appendChild(cardContent);
-    cardContent.appendChild(cardIdElement);
-    cardContent.appendChild(cardTitleElement);
-    cardContent.appendChild(cardDescriptionElement);
-    card.appendChild(actions);
-    actions.appendChild(updateBtn);
-    actions.appendChild(deleteBtn);
-    const parentContainer=null ;
-if(document.getElementById("status").value=='1'){
- document.getElementById('todo').appendChild(card);
-}
-else if(document.getElementById("status").value=='2'){
-  document.getElementById('inprogress').appendChild(card);
- }
- else if(document.getElementById("status").value=='3'){
-  document.getElementById('done').appendChild(card);
- }
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      "title":  document.getElementById('cardname').value,
+      "description": document.getElementById('carddescription').value,
+      "section": document.getElementById("status").value
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("http://localhost:8080/api/boards/1/cards", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
    
  cardpopupForm.style.display = 'none';
 });
+
+// Function addCardToHtml(){
+  
+//   const card = document.createElement('div');
+//   card.classList.add('card');
+
+//   const stateBar = document.createElement('div');
+//   stateBar.classList.add('state-bar');
+
+//   const cardContent = document.createElement('div');
+//   cardContent.classList.add('card-content');
+
+//   const cardIdElement = document.createElement('div');
+//   cardIdElement.classList.add('card-id');
+//   cardIdElement.innerText = '#' ;
+
+//   const cardTitleElement = document.createElement('h3');
+//   cardTitleElement.classList.add('card-title');
+//   cardTitleElement.innerText = document.getElementById('cardname').value.trim();
+
+//   const cardDescriptionElement = document.createElement('p');
+//   cardDescriptionElement.classList.add('card-description');
+ 
+//   cardDescriptionElement.innerText = document.getElementById('carddescription').value;
+
+//   const actions = document.createElement('div');
+//   actions.classList.add('actions');
+
+//   const updateBtn = document.createElement('button');
+//   updateBtn.classList.add('update-btn');
+//   updateBtn.innerText = 'Update';
+
+//   const deleteBtn = document.createElement('button');
+//   deleteBtn.classList.add('delete-btn');
+//   deleteBtn.innerText = 'Delete';
+
+//   // Append all elements to the card
+//   card.appendChild(stateBar);
+//   card.appendChild(cardContent);
+//   cardContent.appendChild(cardIdElement);
+//   cardContent.appendChild(cardTitleElement);
+//   cardContent.appendChild(cardDescriptionElement);
+//   card.appendChild(actions);
+//   actions.appendChild(updateBtn);
+//   actions.appendChild(deleteBtn);
+// if(document.getElementById("status").value=='1'){
+// document.getElementById('todo').appendChild(card);
+// }
+// else if(document.getElementById("status").value=='2'){
+// document.getElementById('inprogress').appendChild(card);
+// }
+// else if(document.getElementById("status").value=='3'){
+// document.getElementById('done').appendChild(card);
+// }
+// }
